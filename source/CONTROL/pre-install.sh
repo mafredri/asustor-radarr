0b7f22e44d1f9673033141733f2081e4153e296d
Mathias Fredriksson <mafredri@gmail.com>
2017-07-29 01:37:14 +0300

    Fix (un)install scripts for ADM 3.0

--- source/CONTROL/pre-install.sh
+++ source/CONTROL/pre-install.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
 
 if [ -z "$APKG_PKG_DIR" ]; then
-    PKG_DIR=/usr/local/AppCentral/radarr
+	PKG_DIR=/usr/local/AppCentral/radarr
 else
-    PKG_DIR=$APKG_PKG_DIR
+	PKG_DIR=$APKG_PKG_DIR
 fi
 
-# Source env variables
-. "${PKG_DIR}/CONTROL/env.sh"
-
 case "${APKG_PKG_STATUS}" in
-    install)
-        ;;
-    upgrade)
-        # Back up configuration
-        rsync -ra --exclude "config_base.xml" "$PKG_CONF/" "$APKG_TEMP_DIR/config/"
+	install)
+		;;
+	upgrade)
+		# Source env variables
+		. "${PKG_DIR}/CONTROL/env.sh"
 
-        # Back up application
-        if [ -d "$PKG_DIR/Radarr" ]; then
-            cp -af "$PKG_DIR/Radarr" "$APKG_TEMP_DIR/"
-        fi
-        ;;
-    *)
-        ;;
+		# Back up configuration
+		rsync -ra --exclude "config_base.xml" "$PKG_CONF/" "$APKG_TEMP_DIR/config/"
+
+		# Back up application
+		if [ -d "$PKG_DIR/Radarr" ]; then
+			cp -af "$PKG_DIR/Radarr" "$APKG_TEMP_DIR/"
+		fi
+		;;
+	*)
+		;;
 esac
 
 exit 0
